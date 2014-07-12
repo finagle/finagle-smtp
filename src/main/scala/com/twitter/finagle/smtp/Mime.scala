f8fe27edcb7134c9330bccfcb78b1f77fafb5e9a
suncelesta <suncelesta@gmail.com>
2014-07-12 23:22:12 +0400

    EmailBuilder.attach + tests + more for headers
    Refined structure, added relevant tests. Content-Type can be parsed from
    string. Files can be attached easier with EmailBuilder.attach.

--- src/main/scala/com/twitter/finagle/smtp/Mime.scala
+++ src/main/scala/com/twitter/finagle/smtp/Mime.scala
@@ -3,113 +3,96 @@ package com.twitter.finagle.smtp
 import java.nio.charset.Charset
 
 /*A trait for general MIME messages*/
-sealed trait Mime {
+sealed trait Mime{
   val version = "1.0"
   val headers: Map[String, String] //headers not including MIME-Version
-  def allHeaders = headers.updated("MIME-Version", version)  
+
+  def allHeaders = headers.updated("MIME-Version", version)
   def getMimeHeaders: Seq[String] = {
     allHeaders map {
       case (k, v) => "%s: %s".format(k, v)
     }
-  }.toSeq
+  }.toSeq.sortWith((s1, s2) => s1 == ("MIME-Version: %s" format version))
 
 
   /*Returns the body of the message that should be sent after headers*/
   def message: String
 
-  def contentTransferEncoding = headers.getOrElse("Content-Transfer-Encoding", TransferEncoding.default)
-  def contentDisposition = headers.getOrElse("Content-Disposition", ContentDisposition.default)
+  def contentTransferEncoding: String = headers.getOrElse("Content-Transfer-Encoding", TransferEncoding.default.value)
+  def contentDisposition: String = headers.getOrElse("Content-Disposition", ContentDisposition.default.value)
+  def contentType: String = headers.getOrElse("Content-Type", ContentType.default.value)
 
   def addHeader(key: String, value: String): Mime
+  def addHeader(header: MimeHeader): Mime
   def addHeaders(newHeaders: Map[String, String]): Mime
+  def addHeaders(newHeaders: Seq[MimeHeader]): Mime
   }
+
 
 //TODO: various shortcuts for common media types
 
 object Mime {
-  def plainText(text: String, enc: Charset) = MimePart(text.getBytes(enc),
-    Map { "Content-Type" -> ("text/plain;charset="+enc.displayName()) }
-  )
-  def plainText(text: String) = MimePart(text.getBytes(Charset.forName("US-ASCII")),
-    Map { "Content-Type" -> ("text/plain") }
-  )
+  val empty = MimePart.empty  
+  
+  def plainText(text: String, enc: Charset) = MimePart(text.getBytes(enc)) setContentType {
+                                                  ContentType("text", "plain", Map("charset" -> enc.displayName()))
+                                              }
+
+  def plainText(text: String, encName: String) = MimePart(text.getBytes(Charset.forName(encName))) setContentType {
+                                                  ContentType("text", "plain", Map("charset" -> encName))
+                                              }
+
+  def plainText(text: String) = MimePart(text.getBytes(Charset.forName("US-ASCII"))) setContentType ContentType("text", "plain")
+}
+
+/*A simple MIME message with some content*/
+case class MimePart(content: Array[Byte], headers: Map[String, String] = Map.empty) extends Mime {
+  def message = new String(content, "US-ASCII") //TODO: 8bit
+
+  def addHeader(key: String, value: String) = copy(headers = this.headers.updated(key, value))
+  def addHeader(header: MimeHeader) = copy(headers = this.headers.updated(header.name, header.value))
+  def addHeaders(newHeaders: Seq[MimeHeader]) = copy(headers = this.headers ++ newHeaders.map(h => (h.name, h.value)))
+  def addHeaders(newHeaders: Map[String, String]) = copy(headers = this.headers ++ newHeaders)
+
+  def setContentType(ct: ContentType) = addHeader(ct)
+  def setContentTransferEncoding(te: TransferEncoding) = addHeader(te)
+  def setContentDisposition(cd: ContentDisposition) = addHeader(cd)
 }
 
 object MimePart {
   val empty = MimePart(Array.empty)
 }
 
-case class MimePart(content: Array[Byte], headers: Map[String, String] = Map.empty) extends Mime {
-  def message = String.copyValueOf(content.map(_.toChar))
+/*A multipart MIME message*/
 
-  def addHeader(key: String, value: String) = copy(content, headers.updated(key, value))
-  def addHeaders(newHeaders: Map[String, String]) = copy(content, headers ++ newHeaders)
-
-  def setContentType(ct: ContentType) = addHeader("Content-Type", ct.toString)
-  def setTransferEncoding(te: String) = addHeader("Content-Transfer-Encoding", te)
-  def setContentDisposition(cd: ContentDisposition) = addHeader("Content-Disposition", cd.toString)
-}
-
-case class MimeMultipart(parts: Seq[MimePart], headers: Map[String, String])(implicit val boundary: String = "boundary")
+case class MimeMultipart(parts: Seq[MimePart], headers: Map[String, String] = Map.empty)(implicit val boundary: String = "d5f6s8asdkfh3")
   extends Mime {
-  val multiContentType = ContentType("multipart", "mixed", Map("boundary" -> boundary))
-  val richBoundary = "--%s--".format(boundary)
+  private val multiContentType = ContentType("multipart", "mixed", Map("boundary" -> boundary))
+  val delimiter = "--%s" format boundary
+  val closingDelimiter = "--%s--" format boundary
 
-  override def allHeaders = super.allHeaders.updated("Content-Type", multiContentType.toString)
+  override def allHeaders = super.allHeaders.updated("Content-Type", multiContentType.value)
+
   def addHeader(key: String, value: String) = copy(headers = this.headers.updated(key, value))
+  def addHeader(header: MimeHeader) = copy(headers = this.headers.updated(header.name, header.value))
+  def addHeaders(newHeaders: Seq[MimeHeader]) = copy(headers = this.headers ++ newHeaders.map(h => (h.name, h.value)))
   def addHeaders(newHeaders: Map[String, String]) = copy(headers = this.headers ++ newHeaders)
   
   def message: String = {
-    val partHeaders = parts map {_.getMimeHeaders mkString "\r\n" }
+    val partHeaders = parts map {_.getMimeHeaders.filter(!_.startsWith("MIME-Version")) mkString "\r\n" }
     val partMessages = parts map { _.message }
-    val partStrings = (partHeaders zip partMessages) map { case (h, m) => "\r\n%s\r\n%s\r\n".format(h, m)}
-    partStrings.mkString(richBoundary, richBoundary, richBoundary)
+    val partStrings = (partHeaders zip partMessages) map { case (h, m) => "\r\n%s\r\n\r\n%s\r\n".format(h, m)}
+    partStrings.mkString(delimiter, delimiter, closingDelimiter)
   }
 
   def addPart(part: MimePart) = copy(parts = this.parts :+ part)
+  def addParts(newparts: Seq[MimePart]) = copy(parts = this.parts ++ newparts)
   def + (part: MimePart) = addPart(part)
 
   def setBoundary(bnd: String) = copy()(bnd)
 }
 
 object MimeMultipart {
-  val empty = MimeMultipart(Seq.empty, Map.empty)
-  def apply(part: MimePart): MimeMultipart = MimeMultipart(Seq(part), Map.empty)
-}
-
-object TransferEncoding {
-  val sevenBit = "7bit"
-  val eightBit = "8bit"
-  val quotedPrintable = "quoted-printable"
-  val binary = "binary"
-  val base64 = "base64"
-
-  val default = sevenBit
-}
-
-trait HeaderWithParams {
-  val params: Map[String, String]
-  def paramsString = {
-    if (params.isEmpty) ""
-    else params
-        .map {case (k: String, v: String) => k + "=" + v}
-        .mkString(";", ", ", "")
-  }
-}
-
-case class ContentType(mediatype: String, subtype: String, params: Map[String, String] = Map.empty)
- extends HeaderWithParams {
-  override def toString = mediatype + "/" + subtype + paramsString
-}
-
-object ContentDisposition {
-  val Inline = "inline"
-  val Attachment = "attachment"
-
-  val default = ContentDisposition(Inline)
-}
-
-case class ContentDisposition(pres: String, params: Map[String, String] = Map.empty)
- extends HeaderWithParams {
-  override def toString = pres + paramsString
+  val empty = MimeMultipart(Seq.empty)
+  def wrap(part: MimePart): MimeMultipart = MimeMultipart(Seq(part))
 }
---
+++ src/test/scala/com/twitter/finagle/smtp/MimeTest.scala
@@ -0,0 +1,92 @@
+package com.twitter.finagle.smtp
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import java.nio.charset.Charset
+
+@RunWith(classOf[JUnitRunner])
+class MimeTest extends FunSuite{
+  def hasOneHeader(msg: Mime, header: String) = msg.getMimeHeaders.count(_ == header) == 1
+
+  test("simple headers") {
+    val mime = Mime.plainText("test")
+    assert(mime.getMimeHeaders contains "Content-Type: text/plain")
+  }
+
+  test("headers with parameters") {
+    val mime = Mime.plainText("test", Charset.forName("UTF-8"))
+    assert(mime.getMimeHeaders contains "Content-Type: text/plain; charset=\"UTF-8\"")
+  }
+
+  test("Content-Type structure") {
+    val ct = ContentType("image", "jpg", Map("x-some-param" -> "value"))
+    val mime = Mime.empty.setContentType(ct)
+
+    assert(mime.getMimeHeaders contains "Content-Type: image/jpg; x-some-param=\"value\"")
+  }
+
+  test("Content-Disposition structure") {
+    val cd = ContentDisposition.attachment("file.png")
+    val mime = Mime.empty.setContentDisposition(cd)
+
+    assert(mime.getMimeHeaders contains "Content-Disposition: attachment; filename=\"file.png\"")
+  }
+
+  test("MIME-Version is the first header") {
+    val headers: Seq[MimeHeader] = Seq(TransferEncoding.Base64, ContentType("application", "octet-stream"), ContentDisposition.inline)
+    val mime = Mime.empty addHeaders headers
+
+    assert(mime.getMimeHeaders.head === "MIME-Version: 1.0")
+  }
+
+  test("Headers are printed in the same order as they were added") {
+    val headers: Seq[MimeHeader] = Seq(TransferEncoding.Base64, ContentType("application", "octet-stream"), ContentDisposition.inline)
+
+    val straightOrder = Mime.empty addHeaders headers
+
+    assert(straightOrder.getMimeHeaders === Seq("MIME-Version: 1.0",
+                                                "Content-Transfer-Encoding: base64",
+                                                "Content-Type: application/octet-stream",
+                                                "Content-Disposition: inline"))
+
+    val reverseOrder = Mime.empty addHeaders headers.reverse
+
+    assert(reverseOrder.getMimeHeaders === Seq("MIME-Version: 1.0",
+                                               "Content-Disposition: inline",
+                                               "Content-Type: application/octet-stream",
+                                               "Content-Transfer-Encoding: base64"))
+  }
+
+  test("Multipart MIME is rendered correctly") {
+    val text = Mime.plainText("This is a test message.")
+    val html = MimePart("<html><h1>This is also a test</h1></html>".getBytes("UTF-8"))
+               .setContentTransferEncoding(TransferEncoding.EightBit)
+               .setContentType(ContentType("text", "html"))
+
+    val attachment = Mime.plainText("This text is attached")
+                      .setContentDisposition(ContentDisposition.attachment("file.txt"))
+
+    val multipart = MimeMultipart(Seq(text, html, attachment))("bnd")
+
+    val expectedMessage = Seq(
+    "--bnd",
+    "Content-Type: text/plain",
+    "",
+    "This is a test message.",
+    "--bnd",
+    "Content-Transfer-Encoding: 8bit",
+    "Content-Type: text/html",
+    "",
+    "<html><h1>This is also a test</h1></html>",
+    "--bnd",
+    "Content-Type: text/plain",
+    "Content-Disposition: attachment; filename=\"file.txt\"",
+    "",
+    "This text is attached",
+    "--bnd--"
+    ) mkString "\r\n"
+
+    assert(multipart.message === expectedMessage)
+  }
+}
