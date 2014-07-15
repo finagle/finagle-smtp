a636dfda5c1f8d586226258e8dea4c8678da9ce0
suncelesta <suncelesta@gmail.com>
2014-07-16 02:41:34 +0400

    Refined structure and Message size declaration

--- src/main/scala/com/twitter/finagle/smtp/filter/TestHelloFilter.scala
+++ src/main/scala/com/twitter/finagle/smtp/filter/TestHelloFilter.scala
@@ -7,16 +7,9 @@ import com.twitter.util.Future
 
 object TestHelloFilter extends SimpleFilter[Request, Reply]{
   def apply(request: Request, service: Service[Request, Reply]) = request match {
-    case Request.Hello => service(request) flatMap {
-      case ok: OK => {
-        val ext = new Extensions(ok.info) {
-        override val isMultiline = ok.isMultiline
-        override val lines = ok.lines
-      }
-        Future.value(ext)}
-    }
-
+    case Request.Hello => service(request)
     case Request.Quit => service(request)
+
     case _ => Future.exception(new RequestNotAllowed)
   }
 }
